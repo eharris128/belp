f013f9a2a1e57a7ddc987116eb5b59859d019a37
Evan Harris <echarris@smcm.edu>
2017-07-17 17:09:05 -0400

    adds more pull request work

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/restaurants/:id', (req, res) => {
         res.status(500).json({message: 'Internal server error'})
     });
 });
-
+// makes another comment
 
 app.post('/restaurants', (req, res) => {
 
