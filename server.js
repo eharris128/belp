90eb1b7d801237828fb892e440654cd7f3205729
52lions06 <mckelvey.william@yahoo.com>
2017-07-17 13:57:32 -0500

    travis cl additions 2

--- server.js
+++ server.js
@@ -155,7 +155,6 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
   });
 }
 
-
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
 function closeServer() {
