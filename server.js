6818fb88d0ae5c616e83ddde663c6e98708deec4
Evan Harris <echarris@smcm.edu>
2017-07-19 12:48:35 -0400

    Adds authentication to put request

--- server.js
+++ server.js
@@ -192,35 +192,37 @@ app.post('/beers', (req, res) => {
 });
 
 
-app.put('/beers/:id', (req, res) => {
+app.put('/beers/:id', 
+  passport.authenticate('basic', {session: false}),
+  (req, res) => {
   // ensure that the id in the request path and the one in request body match
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      const message = (
+        `Request path id (${req.params.id}) and request body id ` +
       `(${req.body.id}) must match`);
-    console.error(message);
-    res.status(400).json({message: message});
-  }
-
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
-  const toUpdate = {};
-  const updateableFields = ['name', 'style', 'description', 'reviews', 'brewery', 'ibu'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
+      console.error(message);
+      res.status(400).json({message: message});
     }
-  });
 
-  Beer
+    // we only support a subset of fields being updateable.
+    // if the user sent over any of the updatableFields, we udpate those values
+    // in document
+    const toUpdate = {};
+    const updateableFields = ['name', 'style', 'description', 'reviews', 'brewery', 'ibu'];
+
+    updateableFields.forEach(field => {
+      if (field in req.body) {
+        toUpdate[field] = req.body[field];
+      }
+    });
+
+    Beer
     // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .exec()
-    .then(beer => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-});
+      .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+      .exec()
+      .then(beer => res.status(204).end())
+      .catch(err => res.status(500).json({message: 'Internal server error'}));
+  });
 
 app.delete('/beers/:id', (req, res) => {
   Beer
