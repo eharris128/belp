8e9542e435e9510466f0187a312d6db470163a18
Evan Harris <echarris@smcm.edu>
2017-07-20 12:39:06 -0400

    Functional population of reviews with appropriate author object, and all key value pairs

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const {Beer} = require('./models.js');
+const mongoose = require('mongoose');
+const { DATABASE_URL } = require('./config');
+mongoose.connect(DATABASE_URL, () => {
+
+  Beer
+    .find({})
+    .populate('reviews.author')
+    .then(function(beers){
+      console.log('======', JSON.stringify(beers, null, 4));
+    });
+});
