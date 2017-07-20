6630c1e570dceea24b5a3c6c73db08bce772e0c4
52lions06 <mckelvey.william@yahoo.com>
2017-07-20 14:51:34 -0500

    imported .env package

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
+ 
+
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/beer1';
+	                'mongodb://localhost/beer1';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-beer1');
