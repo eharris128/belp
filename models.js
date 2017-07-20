4ca0da6ec8f8c9b883126bdba84ba6a647dedc5f
Evan Harris <echarris@smcm.edu>
2017-07-20 13:47:39 -0400

    Cleans up comments and rearranges user schema

--- models.js
+++ models.js
@@ -2,7 +2,9 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-// this is our schema to represent a beer
+
+// Beer related functions
+
 const beerSchema = mongoose.Schema({
   name: {type: String, required: true},
   abv: {type: Number, required: true},
@@ -17,41 +19,11 @@ const beerSchema = mongoose.Schema({
   }]
 });
 
-// myuser123 ; password
-const UserSchema = mongoose.Schema({
-  // _creator: { type: Number, ref: 'Beer'},
-  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Beer'}],
-  username:{
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  firstName: {type: String, default: ''},
-  lastName: {type: String, default: ''}
-});
-
-// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
-// allow us to define properties on our object that manipulate
-// properties that are stored in the database. Here we use it
-// to generate a human readable string based on the address object
-// we're storing in Mongo.
-
-// beerSchema.virtual('').get(function() {
-//   return `${this.address.building} ${this.address.street}`.trim();});
-
-// this virtual grabs the most recent grade for a restaurant.
 beerSchema.virtual('review').get(function() {
   const reviewObj = this.reviews.sort((a, b) => {return b.date - a.date;})[0] || {};
   return reviewObj.review;
 });
 
-// this is an *instance method* which will be available on all instances
-// of the model. This method will be used to return an object that only
-// exposes *some* of the fields we want from the underlying data
 beerSchema.methods.apiRepr = function() {
 
   return {
@@ -67,6 +39,21 @@ beerSchema.methods.apiRepr = function() {
 };
 
 // User related functions
+
+const UserSchema = mongoose.Schema({
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Beer'}],
+  username:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {type: String, default: ''},
+  lastName: {type: String, default: ''}
+});
 
 UserSchema.methods.apiRepr = function() {
   return {
@@ -88,8 +75,6 @@ UserSchema.statics.hashPassword = function(password) {
     .then(hash => hash);
 };
 
-// note that all instance methods and virtual properties on our
-// schema must be defined *before* we make the call to `.model`.
 const Beer = mongoose.model('Beer', beerSchema);
 const User = mongoose.model('User', UserSchema);
 
