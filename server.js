fdf9611bfdaa2db9bcd596b72c8a270571021504
Evan Harris <echarris@smcm.edu>
2017-07-21 14:41:40 -0400

    Trims comments

--- server.js
+++ server.js
@@ -200,13 +200,8 @@ app.put('/beers/:id',
       console.error(message);
       res.status(400).json({message: message});
     }
-    // we only support a subset of fields being updateable.
-    // if the user sent over any of the updatableFields, we udpate those values
-    // in document
-    console.log('the data format that we want: ' + JSON.stringify(req.body.reviews[0]));
 
     Beer
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
       .findByIdAndUpdate(req.params.id, {$push: {reviews: req.body.reviews[0]}}, {new: true})
       .exec()
       .then(updatedBeer => {
@@ -225,17 +220,12 @@ app.delete('/beers/:id', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
 let server;
 
-// this function connects to our database, then starts the server
 function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
   return new Promise((resolve, reject) => {
@@ -255,8 +245,6 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
   });
 }
 
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
 function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
@@ -271,8 +259,6 @@ function closeServer() {
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 }
